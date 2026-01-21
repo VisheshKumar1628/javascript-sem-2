402608d48046bf96cea7e76a1888fcb3448ac84f
VisheshKumar1628 <visheshkumar3416@gmail.com>
2026-01-21 10:23:44 +0530

    Hello Mr. Carmille

---
+++ DSA/Count.py
@@ -0,0 +1,8 @@
+n=12345
+count=0
+if (n==0):
+    print("count = 1")
+while (n>0):
+    count+=1
+    n=n//10
+print(count)
---
+++ Web Dev Sem-II/Event_Propagation.js
@@ -0,0 +1,7 @@
+const div2=document.querySelector('.outer')
+const div=document.querySelector('.container')
+const btn=document.querySelector('Button')
+
+div2.addEventListener('click', ()=>(console.log(div2)))
+div.addEventListener('click', ()=>(console.log(div)))
+btn.addEventListener('click', ()=>(console.log(btn)))
