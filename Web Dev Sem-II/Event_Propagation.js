87205d1e47a223dcf0c2cff46499eb0b6ad1e9fa
VisheshKumar1628 <visheshkumar3416@gmail.com>
2026-01-21 10:51:25 +0530

    Friends

--- Web Dev Sem-II/Event_Propagation.js
+++ Web Dev Sem-II/Event_Propagation.js
@@ -1,7 +1,10 @@
-const div2=document.querySelector('.outer')
-const div=document.querySelector('.container')
-const btn=document.querySelector('Button')
+// const div2=document.querySelector('.outer')
+// const div=document.querySelector('.container')
+// const btn=document.querySelector('Button')
 
-div2.addEventListener('click', ()=>(console.log(div2)))
-div.addEventListener('click', ()=>(console.log(div)))
-btn.addEventListener('click', ()=>(console.log(btn)))
+// div2.addEventListener('click', ()=>(console.log(div2)))
+// div.addEventListener('click', ()=>(console.log(div)))
+// btn.addEventListener('click', ()=>(console.log(btn)))
+
+console.log("Hey Everyone!")
+
