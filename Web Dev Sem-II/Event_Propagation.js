4bedf45b0e16ccc7c3e66a7b00bc78ee6d5a756a
VisheshKumar1628 <visheshkumar3416@gmail.com>
2026-01-22 10:46:30 +0530

    New Commit

--- Web Dev Sem-II/Event_Propagation.js
+++ Web Dev Sem-II/Event_Propagation.js
@@ -6,5 +6,72 @@
 // div.addEventListener('click', ()=>(console.log(div)))
 // btn.addEventListener('click', ()=>(console.log(btn)))
 
-console.log("Hey Everyone!")
 
+
+
+// console.log("Hey Everyone!")
+
+
+
+// debugger
+// // print();
+// console.log(a)
+// // console.log(b)
+
+// var a=2343;
+// let b=345;
+
+// console.log(a)
+// console.log(b)
+
+// var c=40;
+
+// function print(){
+//     console.log(c)
+//     var c=30
+//     console.log("Inside fn")
+// }
+// print()
+
+
+
+// function first(){
+//     second()
+// }
+
+// function second(){
+//     third()
+// }
+
+// function third(){
+//     console.trace()
+// }
+// first()
+
+
+
+// console.log(b)
+// console.log(a)
+
+// let a=345;
+// var b=3453;
+
+// console.log(a)
+// console.log(b)
+
+
+
+// let total=50
+
+// function calculate(){
+//     console.log(total)
+//     let total=100
+// }
+
+// calculate()
+
+
+
+console.log("Watching Cartoon")
+setTimeout(()=>{console.log("Maggie is ready")},2000)
+console.log("Talking to your friend")
